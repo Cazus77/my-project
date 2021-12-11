9c96e3625951f4396792aecf9e87785ad7c1f3a3
Andrey Privezentcev <9851800@mail.ru>
2021-12-11 19:07:10 +0300

    variant_1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Calendar from "../src/components/Calendar/Calendar";
 import PrevMonthButton from "./components/PrevMounthButton/PrevMounthButton";
 import NextMonthButton from "./components/NextMounthButton/NextMounthButton";
 import DateDifference from "./components/DateDifference/DateDifference";
-import { useState } from "react";
+import React, { useState } from "react";
 import * as calendar from "./components/Calendar";
 
 const defaultData = {
@@ -35,56 +35,33 @@ const objState = {
 
 function App() {
   const [state, setState] = useState(objState);
-  const [selectMonth, setSelectMonth] = useState(state.month);
-  const [selectYear, setSelectYear] = useState(state.year);
+  console.log(state.date);
 
-  const { currentDate, selectedDate } = state;
-  const { years, monthNames, weekDayNames } = defaultData;
-  const monthData = calendar.getMonthData(selectYear, selectMonth);
-  /////////////////////////////////////////////////////////////////////////////////
-  const handleDayClick = (date) => {
-    setState({ ...state, ...{ selectedDate: date } });
-  };
+  const year = state.date.getFullYear();
+  const month = state.date.getMonth();
+
+  ////////////////////////////////////////////////////////////////////////////
   const handlePrevMonthButtonClick = () => {
-    const date = new Date(state.year, state.month - 1);
+    const date = new Date(year, month - 1);
+    setState({ date });
     console.log(date);
+  };
+
+  const handleNextMonthButtonClick = () => {
+    const date = new Date(year, month + 1);
     setState({ date });
   };
-  /////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////
   return (
     <div className="block">
       <h1 className="description">Разность дат по календарю</h1>
       <div className="block__calendar">
-        <PrevMonthButton
-          handlePrevMonthButtonClick={handlePrevMonthButtonClick}
-        />
-        <Calendar
-          defaultData={defaultData}
-          selectMonth={selectMonth}
-          selectYear={selectYear}
-          monthData={monthData}
-          objState={objState}
-          handleDayClick={handleDayClick}
-        />{" "}
-        <Calendar
-          defaultData={defaultData}
-          selectMonth={selectMonth}
-          selectYear={selectYear}
-          monthData={monthData}
-          objState={objState}
-          handleDayClick={handleDayClick}
-        />{" "}
-        <Calendar
-          defaultData={defaultData}
-          selectMonth={selectMonth}
-          selectYear={selectYear}
-          monthData={monthData}
-          objState={objState}
-          handleDayClick={handleDayClick}
-        />
-        <NextMonthButton />
+        <button onClick={handlePrevMonthButtonClick}>{"<"}</button>
+        <Calendar />
+        <Calendar />
+        <Calendar />
+        <button onClick={handleNextMonthButtonClick}>{">"}</button>
       </div>
-      <DateDifference />
     </div>
   );
 }
--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -3,64 +3,126 @@ import "./Calendar.sass";
 import * as calendar from "./index";
 import classnames from "classnames";
 
-function Calendar({
-  defaultData,
-  selectMonth,
-  selectYear,
-  monthData,
-  handleDayClick,
-}) {
-  console.log(defaultData);
-  // console.log(selectYear);
-  // console.log(monthNames[selectMonth]);
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const defaultData = {
+  years: [
+    2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
+    2022, 2023,
+  ],
+  monthNames: [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+  ],
+  weekDayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+};
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const objState = {
+  date: new Date(),
+  year: new Date().getFullYear(),
+  month: new Date().getMonth(),
+  day: new Date().getDate(),
+  currentDate: new Date(),
+  selectedDate: null,
+};
+
+function Calendar() {
+  const [state, setState] = useState(objState);
+
+  const { years, monthNames, weekDayNames } = defaultData;
+  const { currentDate, selectedDate } = state;
+
+  const year = state.date.getFullYear();
+  const month = state.date.getMonth();
+  console.log(month);
+  console.log(state.month);
+  console.log(selectedDate);
+
+  const handleSelectChange = () => {
+    console.log(monthSelect.value);
+    const year = yearSelect.value;
+    const month = monthSelect.value;
+    const date = new Date(year, month);
+    console.log(date);
+
+    setState({ date });
+  };
+  const handleDayClick = (date) => {
+    setState({ ...state, ...{ selectedDate: date } });
+  };
+
+  let monthSelect;
+  let yearSelect;
+
+  const monthData = calendar.getMonthData(year, month);
+  console.log(month);
   return (
-    <>
-      <div className="calendar">
-        <header>
-          <div>
-            <span>{defaultData.monthNames[selectMonth]}</span>
-            <span>{selectYear}</span>
-          </div>
-        </header>
-        <table>
-          <thead>
-            <tr>
-              {defaultData.weekDayNames.map((name) => (
-                <th key={name}>{name}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {monthData.map((week, index) => (
-              <tr key={index} className="week">
-                {week.map((date, index) =>
-                  date ? (
-                    <td
-                      key={index}
-                      className={classnames("day", {
-                        today: calendar.areEqual(date, defaultData.currentDate),
-                        selected: calendar.areEqual(
-                          date,
-                          defaultData.selectedDate
-                        ),
-                      })}
-                      onClick={() => handleDayClick(date)}
-                    >
-                      {date.getDate()}
-                    </td>
-                  ) : (
-                    <td key={index} />
-                  )
-                )}
-              </tr>
+    <div className="calendar">
+      <header>
+        <select
+          ref={(element) => (monthSelect = element)}
+          value={month}
+          onChange={handleSelectChange}
+        >
+          {monthNames.map((name, index) => (
+            <option key={name} value={index}>
+              {name}
+            </option>
+          ))}
+        </select>
+        <select
+          ref={(element) => (yearSelect = element)}
+          value={year}
+          onChange={handleSelectChange}
+        >
+          {years.map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+      </header>
+      <table>
+        <thead>
+          <tr>
+            {weekDayNames.map((name) => (
+              <th key={name}>{name}</th>
             ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+          </tr>
+        </thead>
+
+        <tbody>
+          {monthData.map((week, index) => (
+            <tr key={index} className="week">
+              {week.map((date, index) =>
+                date ? (
+                  <td
+                    key={index}
+                    className={classnames("day", {
+                      today: calendar.areEqual(date, currentDate),
+                      selected: calendar.areEqual(date, selectedDate),
+                    })}
+                    onClick={() => handleDayClick(date)}
+                  >
+                    {date.getDate()}
+                  </td>
+                ) : (
+                  <td key={index} />
+                )
+              )}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
