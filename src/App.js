aca6ca7445d8c411089827154463c867934e5a14
Andrey Privezentcev <9851800@mail.ru>
2021-12-11 20:02:25 +0300

    variant_2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.sass";
 import Calendar from "../src/components/Calendar/Calendar";
-import PrevMonthButton from "./components/PrevMounthButton/PrevMounthButton";
-import NextMonthButton from "./components/NextMounthButton/NextMounthButton";
 import DateDifference from "./components/DateDifference/DateDifference";
 import React, { useState } from "react";
 import * as calendar from "./components/Calendar";
@@ -35,21 +33,36 @@ const objState = {
 
 function App() {
   const [state, setState] = useState(objState);
+
   console.log(state.date);
+  const { years, monthNames, weekDayNames } = defaultData;
+  const { currentDate, selectedDate } = state;
 
   const year = state.date.getFullYear();
   const month = state.date.getMonth();
 
+  // const monthData = calendar.getMonthData(year, month);
+  // console.log(monthData);
   ////////////////////////////////////////////////////////////////////////////
   const handlePrevMonthButtonClick = () => {
     const date = new Date(year, month - 1);
     setState({ date });
-    console.log(date);
   };
 
   const handleNextMonthButtonClick = () => {
     const date = new Date(year, month + 1);
     setState({ date });
+  };
+
+  const handleSelectChange = () => {
+    // const year = yearSelect.value;
+    // const month = monthSelect.value;
+    // const date = new Date(year, month);
+    // console.log(date);
+    // setState({ date });
+  };
+  const handleDayClick = (date) => {
+    setState({ ...state, ...{ selectedDate: date } });
   };
   //////////////////////////////////////////////////////////////////////////
   return (
@@ -57,11 +70,48 @@ function App() {
       <h1 className="description">Разность дат по календарю</h1>
       <div className="block__calendar">
         <button onClick={handlePrevMonthButtonClick}>{"<"}</button>
-        <Calendar />
-        <Calendar />
-        <Calendar />
+        <Calendar
+          year={year}
+          years={years}
+          monthText={monthNames[month - 1]}
+          month={month}
+          handleSelectChange={handleSelectChange}
+          monthNames={monthNames}
+          weekDayNames={weekDayNames}
+          currentDate={currentDate}
+          selectedDate={selectedDate}
+          handleDayClick={handleDayClick}
+          monthData={calendar.getMonthData(year, month - 1)}
+        />
+        <Calendar
+          year={year}
+          years={years}
+          monthText={monthNames[month]}
+          month={month}
+          handleSelectChange={handleSelectChange}
+          monthNames={monthNames}
+          weekDayNames={weekDayNames}
+          currentDate={currentDate}
+          selectedDate={selectedDate}
+          handleDayClick={handleDayClick}
+          monthData={calendar.getMonthData(year, month)}
+        />
+        <Calendar
+          year={year}
+          years={years}
+          monthText={monthNames[month + 1]}
+          month={month}
+          handleSelectChange={handleSelectChange}
+          monthNames={monthNames}
+          weekDayNames={weekDayNames}
+          currentDate={currentDate}
+          selectedDate={selectedDate}
+          handleDayClick={handleDayClick}
+          monthData={calendar.getMonthData(year, month + 1)}
+        />
         <button onClick={handleNextMonthButtonClick}>{">"}</button>
       </div>
+      <DateDifference />
     </div>
   );
 }
--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,94 +1,52 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Calendar.sass";
 import * as calendar from "./index";
 import classnames from "classnames";
 
-const defaultData = {
-  years: [
-    2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
-    2022, 2023,
-  ],
-  monthNames: [
-    "Январь",
-    "Февраль",
-    "Март",
-    "Апрель",
-    "Май",
-    "Июнь",
-    "Июль",
-    "Август",
-    "Сентябрь",
-    "Октябрь",
-    "Ноябрь",
-    "Декабрь",
-  ],
-  weekDayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
-};
+// const defaultData = {
+//   years: [
+//     2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021,
+//     2022, 2023,
+//   ],
+//   monthNames: [
+//     "Январь",
+//     "Февраль",
+//     "Март",
+//     "Апрель",
+//     "Май",
+//     "Июнь",
+//     "Июль",
+//     "Август",
+//     "Сентябрь",
+//     "Октябрь",
+//     "Ноябрь",
+//     "Декабрь",
+//   ],
+//   weekDayNames: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"],
+// };
+//
+// const objState = {
+//   date: new Date(),
+//   year: new Date().getFullYear(),
+//   month: new Date().getMonth(),
+//   day: new Date().getDate(),
+//   currentDate: new Date(),
+//   selectedDate: null,
+// };
 
-const objState = {
-  date: new Date(),
-  year: new Date().getFullYear(),
-  month: new Date().getMonth(),
-  day: new Date().getDate(),
-  currentDate: new Date(),
-  selectedDate: null,
-};
-
-function Calendar() {
-  const [state, setState] = useState(objState);
-
-  const { years, monthNames, weekDayNames } = defaultData;
-  const { currentDate, selectedDate } = state;
-
-  const year = state.date.getFullYear();
-  const month = state.date.getMonth();
-  console.log(month);
-  console.log(state.month);
-  console.log(selectedDate);
-
-  const handleSelectChange = () => {
-    console.log(monthSelect.value);
-    const year = yearSelect.value;
-    const month = monthSelect.value;
-    const date = new Date(year, month);
-    console.log(date);
-
-    setState({ date });
-  };
-  const handleDayClick = (date) => {
-    setState({ ...state, ...{ selectedDate: date } });
-  };
-
-  let monthSelect;
-  let yearSelect;
-
-  const monthData = calendar.getMonthData(year, month);
-  console.log(month);
+function Calendar({
+  year,
+  weekDayNames,
+  currentDate,
+  selectedDate,
+  handleDayClick,
+  monthText,
+  monthData,
+}) {
   return (
     <div className="calendar">
       <header>
-        <select
-          ref={(element) => (monthSelect = element)}
-          value={month}
-          onChange={handleSelectChange}
-        >
-          {monthNames.map((name, index) => (
-            <option key={name} value={index}>
-              {name}
-            </option>
-          ))}
-        </select>
-        <select
-          ref={(element) => (yearSelect = element)}
-          value={year}
-          onChange={handleSelectChange}
-        >
-          {years.map((year) => (
-            <option key={year} value={year}>
-              {year}
-            </option>
-          ))}
-        </select>
+        {monthText}-{year}
       </header>
       <table>
         <thead>
